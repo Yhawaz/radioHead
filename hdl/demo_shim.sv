5e7b44764c175dc58b86ea2033677120928286a5
Yhawaz <yhawaz@mit.edu>
2025-11-15 17:43:16 -0500

    good catch watson

--- hdl/demo_shim.sv
+++ hdl/demo_shim.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 `default_nettype none
-module demodulate #(
+module demo_shim #(
     parameter integer C_S00_AXIS_TDATA_WIDTH = 32,
     parameter integer C_M00_AXIS_TDATA_WIDTH = 32
 )(
@@ -21,43 +21,47 @@ module demodulate #(
     output logic [(C_M00_AXIS_TDATA_WIDTH/8)-1:0] m00_axis_tstrb
 );
 
+	logic meow_ready;
+	logic meow_valid;
+	logic meow_last;
+	logic [C_M00_AXIS_TDATA_WIDTH-1:0] meow_data; 
+	logic [(C_M00_AXIS_TDATA_WIDTH/8)-1:0] meow_strb;
+
 	cordic angle_maker(
 		.s00_axis_aclk(s00_axis_aclk),
-		.s00_axis_arestn(s00_axis_aresetn),
+		.s00_axis_aresetn(s00_axis_aresetn),
 		.s00_axis_tlast(s00_axis_tlast),
 		.s00_axis_tvalid(s00_axis_tvalid),
 		.s00_axis_tdata(s00_axis_tdata),
 		.s00_axis_tstrb(s00_axis_tstrb),
 		.s00_axis_tready(s00_axis_tready),
 
-		.m00_axis_tready(),
-		.m00_axiss_tvalid(),
-		.m00_axis_tlast(),
-		.m00_axis_tdata(),
-		.m00_axis_tstrb(),
+		.m00_axis_tready(meow_ready),
+		.m00_axis_tvalid(meow_valid),
+		.m00_axis_tlast(meow_last),
+		.m00_axis_tdata(meow_data),
+		.m00_axis_tstrb(meow_strb)
 	);
 
 	demodulate dmoder(
 		.s00_axis_aclk(s00_axis_aclk),
-		.s00_axis_arestn(s00_axis_aresetn),
-		.s00_axis_tlast(),
-		.s00_axis_tvalid(),
-		.s00_axis_tdata(),
-		.s00_axis_tstrb(),
-		.s00_axis_tready(),
+		.s00_axis_aresetn(s00_axis_aresetn),
+		.s00_axis_tlast(meow_last),
+		.s00_axis_tvalid(meow_valid),
+		.s00_axis_tdata(meow_data),
+		.s00_axis_tstrb(meow_strb),
+		.s00_axis_tready(meow_ready),
 
-		.m00_axis_tready(),
-		.m00_axiss_tvalid(),
-		.m00_axis_tlast(),
-		.m00_axis_tdata(),
-		.m00_axis_tstrb(),
+		.m00_axis_tready(m00_axis_tready),
+		.m00_axis_tvalid(m00_axis_tvalid),
+		.m00_axis_tlast(m00_axis_tlast),
+		.m00_axis_tdata(m00_axis_tdata),
+		.m00_axis_tstrb(m00_axis_tstrb)
 	);
-
-
-
-
 
 endmodule
 
 
 `default_nettype wire
+
+
