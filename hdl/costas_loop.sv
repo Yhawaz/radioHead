c1be6bf023d0b4b379591afbe5a6b7595c6f90c0
Anthony Acevedo <aace217@mit.edu>
2025-11-15 18:17:17 -0500

    started costas_loop

--- hdl/costas_loop.sv
+++ hdl/costas_loop.sv
@@ -1,4 +1,37 @@
-// outline for the module:
-// use cordic to calculate the I and the Q values of the input with just
-// a zero value at the input y of the incoming cordic module
-// 
+
+`timescale 1ns / 1ps
+`default_nettype none
+module costas_loop #(
+    parameter integer C_S00_AXIS_TDATA_WIDTH = 32,
+    parameter integer C_M00_AXIS_TDATA_WIDTH = 32,
+)(
+    // Ports of Axi Slave Bus Interface S00_AXIS
+    input wire s00_axis_aclk,
+    input wire s00_axis_aresetn,
+    input wire s00_axis_tlast,
+    input wire s00_axis_tvalid,
+    input wire [C_S00_AXIS_TDATA_WIDTH-1:0] s00_axis_tdata,
+    input wire [(C_S00_AXIS_TDATA_WIDTH/8)-1:0] s00_axis_tstrb,
+    output logic s00_axis_tready,
+
+    // Ports of Axi Master Bus Interface M00_AXIS
+    input wire m00_axis_tready,
+    output logic m00_axis_tvalid,
+    output logic m00_axis_tlast,
+    output logic [C_M00_AXIS_TDATA_WIDTH-1:0] m00_axis_tdata, // [15:0] is magnitude (unsigned 16-bit integer). [31:16] is angle.
+    output logic [(C_M00_AXIS_TDATA_WIDTH/8)-1:0] m00_axis_tstrb
+);
+    // outline for the module:
+    // use cordic to calculate the I and the Q values of the input with just
+    // a zero value at the input y of the incoming cordic module
+    // TODO: the data coming into this will have already had the frequency shifted down by 57 kHz
+    // TODO: Then a LPF will be used to filter for the RDS
+
+
+
+
+
+
+endmodule
+
+`default_nettype wire
