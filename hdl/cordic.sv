0e108aa2fa1c7c193b3511930dbcb4bfe6005bb4
Anthony Acevedo <aace217@mit.edu>
2025-11-23 23:06:04 -0500

    SOMETHING IS IGNORING THE SIGN OF THE INPUT TO THE MODULE IN VERILOG SO MY X IS NEGATIVE WHEN IT SHOULD BE POSITIVE AHHHHHHHHHHHHH

--- hdl/cordic.sv
+++ hdl/cordic.sv
@@ -27,8 +27,75 @@ module cordic #(
 
     logic [31:0] angles [0:15];
     logic [31:0] fixed_gain;
+
+    // debug variables
     logic [31:0] angle1;
+    logic signed [31:0] x_i0,x_i1,x_i2,x_i3,x_i4,x_i5,x_i6,x_i7,x_i8,x_i9,x_i10,x_i11,x_i12,x_i13,x_i14,x_i15,x_i16;
+    logic signed [31:0] y_i0,y_i1,y_i2,y_i3,y_i4,y_i5,y_i6,y_i7,y_i8,y_i9,y_i10,y_i11,y_i12,y_i13,y_i14,y_i15,y_i16;
+    logic signed [31:0] z_i0,z_i1,z_i2,z_i3,z_i4,z_i5,z_i6,z_i7,z_i8,z_i9,z_i10,z_i11,z_i12,z_i13,z_i14,z_i15,z_i16;
+
+    always_comb begin
+        // x
+        x_i0 = x_i[0];
+        x_i1 = x_i[1];
+        x_i2 = x_i[2];
+        x_i3 = x_i[3];
+        x_i4 = x_i[4];
+        x_i5 = x_i[5];
+        x_i6 = x_i[6];
+        x_i7 = x_i[7];
+        x_i8 = x_i[8];
+        x_i9 = x_i[9];
+        x_i10 = x_i[10];
+        x_i11 = x_i[11];
+        x_i12 = x_i[12];
+        x_i13 = x_i[13];
+        x_i14 = x_i[14];
+        x_i15 = x_i[15];
+        x_i16 = x_i[16];
+
+        // y
+        y_i0 = y_i[0];
+        y_i1 = y_i[1];
+        y_i2 = y_i[2];
+        y_i3 = y_i[3];
+        y_i4 = y_i[4];
+        y_i5 = y_i[5];
+        y_i6 = y_i[6];
+        y_i7 = y_i[7];
+        y_i8 = y_i[8];
+        y_i9 = y_i[9];
+        y_i10 = y_i[10];
+        y_i11 = y_i[11];
+        y_i12 = y_i[12];
+        y_i13 = y_i[13];
+        y_i14 = y_i[14];
+        y_i15 = y_i[15];
+        y_i16 = y_i[16];
+
+        // z
+        z_i0 = z_i[0];
+        z_i1 = z_i[1];
+        z_i2 = z_i[2];
+        z_i3 = z_i[3];
+        z_i4 = z_i[4];
+        z_i5 = z_i[5];
+        z_i6 = z_i[6];
+        z_i7 = z_i[7];
+        z_i8 = z_i[8];
+        z_i9 = z_i[9];
+        z_i10 = z_i[10];
+        z_i11 = z_i[11];
+        z_i12 = z_i[12];
+        z_i13 = z_i[13];
+        z_i14 = z_i[14];
+        z_i15 = z_i[15];
+        z_i16 = z_i[16];
+    end
+
     assign angle1 = angles[0];
+    // end debug variables
+
     assign fixed_gain = 2608131502;
 
     initial begin
@@ -94,10 +161,14 @@ module cordic #(
     logic tlast_i [0:C_NUM_CORDIC_ITERATIONS];
 
     // Initial inputs (scale the absolute value)
-    logic signed [C_S00_AXIS_TDATA_WIDTH/2-1:0] pre_x_i_0, pre_y_i_0;
+    logic signed [C_S00_AXIS_TDATA_WIDTH/2:0] pre_x_i_0, pre_y_i_0;
     logic signed [C_S00_AXIS_TDATA_WIDTH/2-1:0] x_i_0, y_i_0, z_i_0;
     logic flips_i_0;
     assign pre_x_i_0 = s00_axis_tdata[(C_S00_AXIS_TDATA_WIDTH/2)-1:0];
+    always_comb begin
+        $display("my_signal = %0b", pre_x_i_0);
+    end
+
     assign pre_y_i_0 = s00_axis_tdata[(C_S00_AXIS_TDATA_WIDTH-1):(C_S00_AXIS_TDATA_WIDTH/2)];
     assign x_i_0 = pre_x_i_0<0?-pre_x_i_0:pre_x_i_0;
     assign y_i_0 = pre_x_i_0<0?-pre_y_i_0:pre_y_i_0;
