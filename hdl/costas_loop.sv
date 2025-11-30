e97028f23234fbe63e2c436012ca5cf99602e50b
yhawaz <yhawaz@mit.edu>
2025-11-30 12:23:31 -0500

    okay now this is a half working shell

--- hdl/costas_loop.sv
+++ hdl/costas_loop.sv
@@ -21,13 +21,54 @@ module costas_loop #(
     output logic [C_M00_AXIS_TDATA_WIDTH-1:0] m00_axis_tdata, // [15:0] is magnitude (unsigned 16-bit integer). [31:16] is angle.
     output logic [(C_M00_AXIS_TDATA_WIDTH/8)-1:0] m00_axis_tstrb
 );
-    // outline for the module:
-    // use cordic to calculate the I and the Q values of the input with just
-    // a zero value at the input y of the incoming cordic module
-    // TODO: the data coming into this will have already had the frequency shifted down by 57 kHz
-    // TODO: Then a LPF will be used to filter for the RDS
+
+    // TODO: Then a LPF will be used to filter for the RDS im 
+    //eh ig thats a way to think about it
+
+
+    //im not doing these
     // TODO: Decimate data also
     // TODO: also resample the signal to 19 kHz
+    //^idk wtf this dude is talking about im implementing thepysdr control loop so
+        //rotate incoming iq signal by our "phase" estimate
+        //calculate our error and phase estimate
+    logic [16:0] beta;
+    logic [16:0] alpha;
+    logic [16:0] phase;
+    logic [16:0] freq;
+
+    always_comb begin
+    
+        //TODO FIGURE THIS LINE TF OUT out= s00_axis_tdata*nco(phase);
+        //mixing is a muliplying the signals by their complex values, fuck my CHUNGUS LIFE
+        error = new_i*new_q; 
+    end
+
+    always_ff @(posedge s00_axis_aclk)begin
+       if(!s00_axis_aresetn)begin
+           // don't do anything
+           m00_axis_tvalid <= 0;
+           m00_axis_tdata <= 0;
+           //m00_axis_tstrb <= 0;
+           m00_axis_tlast <= 0;
+       end else begin
+           if(s00_axis_tvalid && s00_axis_tready)begin
+               // grab valid data and compute the difference
+               val_reg <= s00_axis_tdata;
+               m00_axis_tdata <= 
+               m00_axis_tvalid <= 1'b1;
+               m00_axis_tlast <= s00_axis_tlast;
+               m00_axis_tstrb <= 255;
+              //recalc stateful things
+                //there is no way in gods green fucking earth we need to pipeline this im gonna sob 
+               phase<=phase+freq+(alpha*error);
+               freq<=freq+freq+(beta*error);
+           end else begin
+                    m00_axis_tvalid <= 1'b0;
+           end
+       end
+    end
+        
 
 endmodule
 
