d9cd825203b2290d1f5f8f7d8d102082939573be
Yhawaz <yhawaz@mit.edu>
2025-12-01 16:30:24 -0500

    Revert "changed pwm and counter for vivado"
    This reverts commit 48fe3609225990169a5fe52590da35aa7f9e7387.

--- hdl/pwm.sv
+++ hdl/pwm.sv
@@ -1,30 +1,16 @@
-module pwm(   input wire clk,
-              input wire rst,
-              input wire [31:0] data_in,
-              input wire valid_data_in,
-              input wire [3:0] sh_amt,
+`default_nettype none // prevents system from inferring an undeclared logic (good practice)
+
+module pwm(   input wire clk_in,
+              input wire rst_in,
+              input wire [7:0] dc_in,
               output logic sig_out);
  
     logic [31:0] count;
-    logic [7:0] cur_dc;
-    logic [7:0] shifted_dc;
-    counter mc (.clk(clk),
-                .rst(rst),
-                .period(255),
-                .count(count));
-
-    assign shifted_dc = cur_dc >> 2*sh_amt;
-
-
-    always_ff @(posedge clk)begin
-	if(rst)begin
-	   cur_dc <= 0;
-	end else begin
-	   if(count == 254)begin
-		cur_dc <= shifted_dc;
-	   end
-	end
-
-    end	    
-    assign sig_out = count<cur_dc; //very simple threshold check
+    counter mc (.clk_in(clk_in),
+                .rst_in(rst_in),
+                .period_in(100000/255), // number of cycles, not the value of the period in ns
+                .count_out(count));
+    assign sig_out = count<dc_in; //very simple threshold check
 endmodule
+
+`default_nettype wire
