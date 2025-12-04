8038d0b335ad5654c06018eedad8d6b743f61190
yhawaz <yhawaz@mit.edu>
2025-12-03 19:23:31 -0500

    meow

--- sdr/yabi_sdr.py
+++ sdr/yabi_sdr.py
@@ -10,25 +10,49 @@ center_freq = 99.5e6
 
 #  Demod
 x = 0.5 * np.angle(x[0:-1] * np.conj(x[1:])) 
-rawr=x
+demoded_sig=x
 
-# grab 19khz tone
-
-N = len(x)
-f_o = -19e3 # amount we need to shift by
-t = np.arange(N)/sample_rate # time vector
-x = x * np.exp(2j*np.pi*f_o*t) # down shift
-
-# Low-Pass Filter
-taps = firwin(numtaps=101, cutoff=7.5e3, fs=sample_rate)
+# bandpass
+taps = firwin(numtaps=101, cutoff=[17e3,21e3], fs=sample_rate,pass_zero=False)
 x = np.convolve(x, taps, 'valid')
 
-x = x[::10]
-fs = 25e3
+#tripled 
+triple=[]
+for val in x:
+    triple.append(val**3)
 
-#
-fft_data=np.fft.fftshift((fft(rawr)))
+band_pass_taps = firwin(numtaps=101, cutoff=[55e3,59e3], fs=sample_rate,pass_zero=False)
+triple = np.convolve(triple, band_pass_taps, 'valid')
 
-#me struggling to plot a fft
-plt.plot(fft_data*fft_data)
+
+#fix it
+
+orig_57=np.convolve(demoded_sig, band_pass_taps, 'valid')
+
+#mixed_down_57=orig_57*triple
+print(len(orig_57))
+print(len(triple))
+
+orig_57=orig_57[:len(triple)]
+mixed_down=orig_57*triple
+
+
+
+
+
+
+val_to_fft= orig_57 
+
+low_pass_taps = firwin(numtaps=501, cutoff=2e3, fs=sample_rate)
+mixed_down=np.convolve(mixed_down,low_pass_taps,'valid')
+plt.plot(mixed_down)
 plt.show()
+
+#fft plotting i like
+num_samples=len(val_to_fft)
+fft_time=np.fft.rfft(val_to_fft)
+
+freqs=np.fft.rfftfreq(num_samples,d=1/sample_rate)
+plt.plot(freqs,fft_time*fft_time)
+plt.show()
+
